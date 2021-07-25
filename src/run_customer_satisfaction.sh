2e95fc30ab01ca09846ba37da5129a5d0bb232e8
Farnaz Tavakol <60339514+farnaztavakool@users.noreply.github.com>
2021-07-25 15:44:34 +1000

    Delete run_customer_satisfaction.sh

--- src/run_customer_satisfaction.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "You need to have pipenv and python3 installed to run this project" 
-echo "Start data cleaning and modeling for Customer Satisfaction"
-pipenv run python3 customer_satisfaction.py
-
-echo "Done ver
