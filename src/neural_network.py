15a4f3caf705da8735e110dde4f11e0503a89499
farnaztavakool <tavakolfarnaz@gmail.com>
2021-07-27 00:03:58 +1000

    added k fold to find the best size

--- src/neural_network.py
+++ src/neural_network.py
@@ -5,10 +5,21 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import log_loss
 from keras.models import Sequential
 from keras.layers import Dense
-
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import VarianceThreshold
+from feature_engine.selection import DropCorrelatedFeatures
+# possible change to pytorch  
+from sklearn.neural_network import MLPClassifier
+from keras.models import Sequential
+from keras.layers import Dense
 from customer_satisfaction import preprocessData
 
 def train_NN_model():
+   
     df_train = loadData("train.csv")
     df_test = loadData("test.csv")
 
@@ -29,17 +40,22 @@ def train_NN_model():
     X_test = df_train_x[rows_split : n_rows]
     Y_test = y_train[rows_split : n_rows]
 
-    model = build_model(X_train)
-    model.fit(X_train, Y_train)
-    prediction = model.predict(X_test)[:,0]
+    
+    output_dim = findBestOutputSize(df_train_x, y_train)
+    
+    model = build_model(df_train_x.shape[1], output_dim)
+    model.fit(df_train_x, y_train)
+    prediction = model.predict_proba(df_test)[:,0]
+    
+    model2 = build_model(X_train.shape[1], output_dim)
+    model2.fit(X_train, Y_train)
+    prediction2 = model.predict(X_test)[:,0]
 
-    loss = log_loss(Y_test, prediction)
+    loss = log_loss(Y_test, prediction2)
     # print(pd.DataFrame({'ID':ID_test, "Target": prediction}))
     print('loss: ', loss)
 
-
-
-
+    return pd.DataFrame({'ID':ID_test, "Target": prediction})
 
 
 def loadData(path):
@@ -47,12 +63,34 @@ def loadData(path):
     return pd.DataFrame(df)
 
 
-def build_model(data):
+def build_model(input_dim, output_dim):
     model = Sequential()
-    model.add(Dense(219,input_dim = data.shape[1], kernel_initializer='uniform', activation = 'tanh'))
-    model.add(Dense(1,input_dim = 219, kernel_initializer='uniform', activation = 'sigmoid'))
+    model.add(Dense(output_dim,input_dim = input_dim, kernel_initializer='uniform', activation = 'tanh'))
+    model.add(Dense(1,input_dim = output_dim, kernel_initializer='uniform', activation = 'sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam')   
     return model
 
+# use cross fold to get the accuracy of the model with different size 
+def findBestOutputSize(data, y):
+    data = data.to_numpy()
+    y = y.to_numpy()
+    scores = [0] * 10
+    alpha_list = [i+1 for i in range(10)]
+    kfold = KFold(n_splits=10)
+    for alpha in alpha_list:
+        for train, test in kfold.split(data, y):
+            output_dim = getNumberOfNeurons(data, alpha)
+            model = build_model(data[train].shape[1],output_dim)
+            model.fit(data[train], y[train])
+            scores[alpha-1]+=model.evaluate(data[test], y[test], verbose=0)
+        scores[alpha-1] = scores[alpha-1]/10
+    alpha = scores.index(min(scores)) +1
+    return getNumberOfNeurons(data, alpha)
 
-train_NN_model()
+def getNumberOfNeurons(data, alpha ):
+    return (data.shape[0]/(alpha*(data.shape[1]+1)))
+
+
+
+
+train_NN_model().to_csv("nn_output.csv",index=False)
