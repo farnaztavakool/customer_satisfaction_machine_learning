4cdcdbab0a5179047f6fffe584f7438e5df6b0f9
RespectedGentleman <85337779+RespectedGentleman@users.noreply.github.com>
2021-07-29 08:39:05 +0800

    Update run.py

--- src/run.py
+++ src/run.py
@@ -4,4 +4,5 @@ codes = ['KNN', 'neural_network', 'logisticRegression', 'decisionTree']
 if not os.path.exists('images'):
     os.makedirs('images')
 for i in codes:
+    print(f'-------------------{i}: -------------------')
     os.system(f'python3 {i}.py')
