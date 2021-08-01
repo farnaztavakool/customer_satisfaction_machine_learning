8ef127f9778dc68b2016d523b16363527477729a
RespectedGentleman <85337779+RespectedGentleman@users.noreply.github.com>
2021-08-01 10:45:04 +0800

    Update data_analysis.py

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -3,33 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import f_classif, VarianceThreshold
 
+def main():
+    train_data = pd.read_csv('train.csv')
+    test_data = pd.read_csv('test.csv')
 
+    # print(train_data.describe())
 
-train_data = pd.read_csv('train.csv')
-test_data = pd.read_csv('test.csv')
+    # print out number of output for each category
+    # also plot 
+    features = train_data.drop(['ID', 'TARGET'], axis=1)
+    y = train_data['TARGET']
+    print(train_data['TARGET'].value_counts())
+    train_data['TARGET'].value_counts().plot.pie(explode=[0,0.1],autopct='%1.1f%%',shadow=True)
+    plt.savefig('images/data_analysis_1.png', bbox_inches='tight')
 
-# print(train_data.describe())
+    # drop constant features
+    constant_filter = VarianceThreshold(threshold= 0.001)
+    features = pd.DataFrame(constant_filter.fit_transform(features))
 
-# print out number of output for each category
-# also plot 
-features = train_data.drop(['ID', 'TARGET'], axis=1)
-y = train_data['TARGET']
-print(train_data['TARGET'].value_counts())
-train_data['TARGET'].value_counts().plot.pie(explode=[0,0.1],autopct='%1.1f%%',shadow=True)
-plt.savefig('images/data_analysis.png', bbox_inches='tight')
-plt.show()
+    f_score = f_classif(features, y)
 
-# drop constant features
-constant_filter = VarianceThreshold(threshold= 0.001)
-features = pd.DataFrame(constant_filter.fit_transform(features))
+    p_values=pd.Series(f_score[0])
 
-f_score = f_classif(features, y)
-
-p_values=pd.Series(f_score[0])
-
-p_values.sort_values(ascending=False).plot.bar(figsize=(20, 8))
-plt.show()
-
-
-
-
+    p_values.sort_values(ascending=False).plot.bar(figsize=(20, 8))
+    plt.savefig('images/data_analysis_2.png', bbox_inches='tight')
